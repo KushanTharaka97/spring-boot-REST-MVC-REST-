4a2aed27b5bc134ba436dee26527c2b968b62fa9
KushanTharaka97 <tharakakushan@gmail.com>
2021-08-04 03:57:47 +0530

    ADD : LIMIT ACCESS API [0002]

--- src/main/java/com/example/ec/repo/TourRepository.java
+++ src/main/java/com/example/ec/repo/TourRepository.java
@@ -3,6 +3,8 @@ package com.example.ec.repo;
 import com.example.ec.domain.Tour;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.awt.print.Pageable;
 import java.util.List;
@@ -14,6 +16,32 @@ import java.util.List;
  * Created by Mary Ellen Bowman
  * --Kushan I am following her course.
  */
+
+@RepositoryRestResource(collectionResourceRel = "packages",path = "packages") //can change the URL path from this
 public interface TourRepository extends PagingAndSortingRepository<Tour, Integer> {
     List<Tour> findByTourPackage(String code, Pageable pageable);
+
+    @Override
+    @RestResource(exported = false) //in this we can hide it to the public
+    <S extends Tour> S save(S s);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends Tour> Iterable<S> saveAll(Iterable<S> iterable);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteById(Integer integer);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(Tour tour);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll(Iterable<? extends Tour> iterable);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll();
 }
